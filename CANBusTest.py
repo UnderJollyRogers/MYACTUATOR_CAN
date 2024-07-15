7f8aab0827b678824c50150c626854dcabe0fa27
UnderJollyRogers <jsanjuan@uwm.edu>
2024-07-15 16:00:38 -0500

    Documentation is a library with the PDF documentation of the CAN protocol

--- CANBusTest.py
+++ CANBusTest.py
@@ -3,10 +3,10 @@ import can
 
 bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=500000)
 
-msg = can.Message(arbitration_id=0x141, data=[0xA2, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00], 
+msg = can.Message(arbitration_id=0x141, data=[0xA2, 0x00, 0x00, 0x00, 0x10, 0x0, 0x01, 0x00], 
                   is_extended_id=False)
 
-speed_decimal = 0x10 | (0x27 << 8) | (0x00 << 16) | (0x00 << 24)
+speed_decimal = 0x20 | (0x27 << 8) | (0x00 << 16) | (0x00 << 24)
 RPM_actual = speed_decimal * 0.01 / 6
 print(RPM_actual)
 
